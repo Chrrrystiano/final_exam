4f84b39d5a3ad1bd1dc1d998c1c982256c9eead7
Chrrrystiano <pefekbiznes@gmail.com>
2024-04-07 03:34:02 +0200

    Lekkie zmiany, wyrzucona kafka z docker-compose, lekkie zmiany w CsvFileImportService oraz porządki w pom.xml i application-properties (wyrzucone duplikaty i zbędne graty)

--- src/main/java/com/example/exam/jdbc/status/ImportFileStatusService.java
+++ src/main/java/com/example/exam/jdbc/status/ImportFileStatusService.java
@@ -1,7 +1,6 @@
 package com.example.exam.jdbc.status;
 
 import com.example.exam.exceptions.ImportStatusNotFoundException;
-import com.example.exam.jdbc.CsvFileImportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,23 +35,8 @@ public class ImportFileStatusService {
         return savedStatus;
     }
 
-//    @Transactional(propagation = Propagation.REQUIRES_NEW)
-//    public void updateImportStatus(String taskId, Status status, int processedRows) {
-//        logger.info("Aktualizacja statusu importu, Task ID: {}", taskId);
-//
-//        ImportFileStatus importFileStatus = importFileStatusRepository.findByTaskId(taskId).orElseThrow(() -> new IllegalArgumentException("Invalid import file status taskId: " + taskId));
-//        if (status != Status.IN_PROGRESS || importFileStatus.getEndTime() == null) {
-//            importFileStatus.setStatus(status);
-//            importFileStatus.setProcessedRows(processedRows);
-//            if (status != Status.IN_PROGRESS) {
-//                importFileStatus.setEndTime(LocalDateTime.now());
-//            }
-//            importFileStatusRepository.save(importFileStatus);
-//        }
-//    }
-
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void updateImportStatus(String taskId, int processedRows) {
+    public void updateImportStatusProcessedRows(String taskId, int processedRows) {
         ImportFileStatus importFileStatus = importFileStatusRepository.findByTaskId(taskId)
                 .orElseThrow(() -> new ImportStatusNotFoundException("Cannot find ImportStatus with taskId: " + taskId));
         importFileStatus.setProcessedRows(processedRows);
@@ -61,15 +45,15 @@ public class ImportFileStatusService {
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void finishImport(String taskId, int processedRows, boolean succes) {
+    public void finishImport(String taskId,boolean succes) {
         ImportFileStatus importFileStatus = importFileStatusRepository.findByTaskId(taskId)
                 .orElseThrow(() -> new ImportStatusNotFoundException("Cannot find ImportStatus with taskId: " + taskId));
-        importFileStatus.setProcessedRows(processedRows);
         importFileStatus.setEndTime(LocalDateTime.now());
         importFileStatus.setStatus(succes ? Status.COMPLETED : Status.REJECTED);
         importFileStatusRepository.save(importFileStatus);
     }
 
+    @Transactional(readOnly = true)
     public ImportFileStatus getImportFileStatus(String taskId) {
         return importFileStatusRepository.findByTaskId(taskId)
                 .orElseThrow(() -> new ImportStatusNotFoundException("Cannot find ImportStatus with taskId: " + taskId));
