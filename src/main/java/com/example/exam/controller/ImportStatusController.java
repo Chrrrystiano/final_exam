660eb67762160695e8d43e3b8e31b363bce574c9
Chrrrystiano <pefekbiznes@gmail.com>
2024-04-07 04:01:49 +0200

    ImportStatusController update

--- src/main/java/com/example/exam/controller/ImportStatusController.java
+++ src/main/java/com/example/exam/controller/ImportStatusController.java
@@ -1,28 +1,28 @@
-//package com.example.exam.controller;
-//
-//import com.example.exam.kafka.ImportStatus;
-//import com.example.exam.kafka.service.ImportStatusService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/import")
-//public class ImportStatusController {
-//    private final ImportStatusService importStatusService;
-//
-//    @Autowired
-//    public ImportStatusController(ImportStatusService importStatusService) {
-//        this.importStatusService = importStatusService;
-//    }
-//
-//    @GetMapping("/status/{taskId}")
-//    @PreAuthorize("hasRole('ADMIN') or hasRole('IMPORTER')")
-//    public ImportStatus getImportStatus(@PathVariable String taskId) {
-//        return importStatusService.getImportStatus(taskId);
-//    }
-//
-//}
+package com.example.exam.controller;
+
+import com.example.exam.jdbc.status.ImportFileStatus;
+import com.example.exam.jdbc.status.ImportFileStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/import")
+public class ImportStatusController {
+    private final ImportFileStatusService importFileStatusService;
+
+    @Autowired
+    public ImportStatusController(ImportFileStatusService importFileStatusService) {
+        this.importFileStatusService = importFileStatusService;
+    }
+
+    @GetMapping("/status/{taskId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('IMPORTER')")
+    public ImportFileStatus getImportStatus(@PathVariable String taskId) {
+        return importFileStatusService.getImportFileStatus(taskId);
+    }
+
+}
